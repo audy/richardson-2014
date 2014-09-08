4c6ad25a5027fa0f7f484bef0881f7f7761003a1
Austin Davis-Richardson <harekrishna@gmail.com>
2014-09-08 19:09:49 -0400

    changed colors

--- R/lib/ggplot_theme.R
+++ R/lib/ggplot_theme.R
@@ -1,6 +1,8 @@
 #!/usr/bin/env rscript
 library(ggplot2)
+library(ggthemr)
 
+ggthemr('fresh') # use fresh but then change some stuff.
 
 ggplot.theme <- theme(panel.background = element_blank(),
                       strip.background = element_blank(),
@@ -11,7 +13,9 @@ ggplot.theme <- theme(panel.background = element_blank(),
 
 ggplot.pal.scale <- scale_fill_manual(values=c("#999999",
                                                "#FFAB00")
-                                        )
+                                        ) 
+
+ggplot.color <-  scale_colour_manual(values=c("#999999", "#FFAB00"))
 
 
 ggplot.pal.sig <-
